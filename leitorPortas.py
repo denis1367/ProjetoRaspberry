382a96030a6907b383d23e39206cabe7a119cbb7
denis1367 <dns_x1@hotmail.com>
2018-09-19 15:18:44 -0300

    iniciando teste

---
+++ leitorPortas.py
@@ -0,0 +1,18 @@
+from threading import 
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+gpio.setup(26,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(19,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(13,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(6,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(21,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(20,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(16,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(12,gpio.IN, pull_up_down = gpio.PUD_DOWN)
+
+def inputPorta(portaGpio):
+	while true:
+		if(gpio.input(portaGpio) == 1)
+		print ("Luz acesssa")
