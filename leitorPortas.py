847740c61d1bf8cc054c14aa85851e44ed8e552d
denis1367 <dns_x1@hotmail.com>
2018-09-19 16:53:40 -0300

    iniciando teste

--- leitorPortas.py
+++ leitorPortas.py
@@ -1,7 +1,8 @@
 from threading import 
 import RPi.GPIO as gpio
 import time
-
+from datetime import datetime
+import time
 gpio.setmode(gpio.BCM)
 gpio.setup(26,gpio.IN, pull_up_down = gpio.PUD_DOWN)
 gpio.setup(19,gpio.IN, pull_up_down = gpio.PUD_DOWN)
@@ -12,11 +13,33 @@ gpio.setup(20,gpio.IN, pull_up_down = gpio.PUD_DOWN)
 gpio.setup(16,gpio.IN, pull_up_down = gpio.PUD_DOWN)
 gpio.setup(12,gpio.IN, pull_up_down = gpio.PUD_DOWN)
 
+p1=0
+p2=0
+p3=0
+p4=0
+p5=0
+p6=0
+p7=0
+p8=0
+
+def registrando(porta )
+	now = datetime.now()
+	porta = data_e_hora_em_texto = data_e_hora_atuais.strftime(‘%d/%m/%Y %H:%M:%S’)
+
 def inputPorta(portaGpio):
 	while true:
-		if(gpio.input(portaGpio) == 1)
-		print ("Luz acesssa")
-		teste
 
-fasldkfjçlasdkjfklçsajdfçlksjfçlkasdjfçljsakd
-                testei tambem
+		inicio=tempo.time()
+		while(gpio.input(portaGpio) == 1)
+		fim = time.time()	
+		print("fim - inicio")
+
+
+
+# aqui salvar o arquivo
+
+			# if(gpio.input(portaGpio) == 1):
+			# 	print ("Luz acesssa")
+	 	#     else:
+	  #   		print("Luz apagada")
+
